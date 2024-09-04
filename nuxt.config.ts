1d090e7ed393a7d5637adbfdc27a03a34c170963
JayBodade <intern@incquet.com>
2024-09-04 16:45:55 +0530

    Added New Config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,27 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   plugins: ['~/plugins/vuetify.js'],
-  ssr:true,
+  ssr: true,
   build: {
     transpile: ['vuetify'],
   },
   css: [
     'vuetify/styles',
     '@mdi/font/css/materialdesignicons.css',
-  ], 
-  
-  routeRules:{
-    "/product/*":{prerender:true},
-  }
- 
-   
+  ],
 
+  hooks: {
+    async "prerender:routes"(ctx) {
+      const  response  = await fetch("https://fakestoreapi.com/products/");
+      const result = await response.json();
+      for (const page of result) {
+        ctx.routes.add(`/product/${page.id}`);
+      }
+    },
+  },
 })
+
 
